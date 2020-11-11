d41c36699fa3aeb1fed30a03233847905ed8b591
FelixU1 <talk2felix841@gmail.com>
2020-11-11 11:25:30 +0100

    fits commit

---
+++ birthday/views.py
@@ -0,0 +1,9 @@
+import datetime
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    now = datetime.datetime.now()
+    return render(request, "birthday/index.html", {
+        "birthday": now.month == 11 and now.day == 13
+    })
